f39b3e2da984d9e5bbab04e5ff438863c767df7e
zhaozhiyangzzy <32759268+zhaozhiyangzzy@users.noreply.github.com>
2022-06-26 16:55:07 +0800

    Update reg.c
    register info

--- src/isa/riscv32/reg.c
+++ src/isa/riscv32/reg.c
@@ -9,6 +9,11 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
+    int i = 0;
+    for(i=0;i<32;i++){
+        printf("reg[%d]=%d\n",i,cpu.gpr[i]._32);
+    }
+    printf("cpu.pc=%d\n",cpu.pc);
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
