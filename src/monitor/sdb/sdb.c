a8cd7e63be84ddc642b1c0f028c42e90232172a8
zhaozhiyangzzy <32759268+zhaozhiyangzzy@users.noreply.github.com>
2022-08-10 21:00:35 +0800

    Update sdb.c

--- src/monitor/sdb/sdb.c
+++ src/monitor/sdb/sdb.c
@@ -10,6 +10,12 @@ static int is_batch_mode = false;
 void init_regex();
 void init_wp_pool();
 
+/*watch point related*/
+//extern WP;
+WP * new_wp(word_t va,int v);
+void free_wp(int n);
+/*watch point related*/
+
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
   static char *line_read = NULL;
@@ -38,27 +44,52 @@ static int cmd_q(char *args) {
   return -1;
 }
 
-static int cmd_si(char * args){
-  cpu_exec(1);
-  return 0;
-}
-
-static int cmd_info(char * args){
-  isa_reg_display();
-  return 0;
-}
-static int cmd_x(char * args){
-    //printf("mem[]=%d\n",pmem[0]);
-    word_t temp_info = vaddr_read(0x80000000,4);
-    printf("temp_info=%d\n",temp_info);
+static int cmd_help(char *args);
+static int cmd_z(char * args){
+    printf("zzy new cmd\n");
     return 0;
 }
 
-static int cmd_p(char * args){
-  return 0;
+static int cmd_si(char * args){
+    cpu_exec(10);
+    return 0;
 }
-static int cmd_help(char *args);
 
+static int cmd_info(char * args){
+    isa_reg_display();
+    return 0;
+}
+
+static int cmd_x(char * args){
+    //printf("mem[]=%d\n",pmem[0]);
+    word_t var = strtoul(args,NULL,16);
+    word_t temp_info = vaddr_read(var,4);
+    printf("temp_info=%x\n",temp_info);
+    return 0;
+}
+static int cmd_p(char * args){
+    return 0;
+}
+
+static int cmd_w(char * args){
+    //stdlib.h
+    word_t var = strtoul(args,NULL,16);
+    printf("var=%x\n",var);
+    int value = vaddr_read(var,4);
+    printf("value=%d\n",value);
+    WP * temp = new_wp(var,value);
+    int number = temp->NO;
+    printf("watch point number=%d\n",number);
+    return 0;
+}
+
+static int cmd_d(char * args){
+    int n = atoi(args);
+    free_wp(n);
+    
+    printf("watch point number=%d\n",n);
+    return 0;
+}
 static struct {
   const char *name;
   const char *description;
@@ -67,10 +98,13 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "si cmd", cmd_si },
-  { "info", "info cmd", cmd_info },
-  { "p", "info p", cmd_p },
+  { "z", "zzy first cmd",cmd_z},
+  { "si", "si cmd " ,cmd_si},
+  { "info", "info cmd", cmd_info},
+  { "p","p cmd",cmd_p},
   { "x","x--memory info",cmd_x},
+  {"w","add watch point",cmd_w},
+  {"d","delete watch point",cmd_d},
   /* TODO: Add more commands */
 
 };
@@ -106,7 +140,7 @@ void sdb_set_batch_mode() {
 
 void sdb_mainloop() {
   if (is_batch_mode) {
-    printf("enter sdb_mainloop() is_batch_mode");
+    printf("enter sdb_mainloop() is_batch_mode\n");
     cmd_c(NULL);
     return;
   }
@@ -145,6 +179,7 @@ void sdb_mainloop() {
 
 void init_sdb() {
   /* Compile the regular expressions. */
+  //printf("now in init_sdb()\n");
   init_regex();
 
   /* Initialize the watchpoint pool. */
