21604b689cfce4adff006aa1617093e04c8994e6
zhaozhiyangzzy <32759268+zhaozhiyangzzy@users.noreply.github.com>
2022-08-10 21:01:52 +0800

    Update sdb.h

--- src/monitor/sdb/sdb.h
+++ src/monitor/sdb/sdb.h
@@ -4,5 +4,14 @@
 #include <common.h>
 
 word_t expr(char *e, bool *success);
+typedef struct watchpoint {
+  int NO;
+  struct watchpoint *next;
+
+  /* TODO: Add more members if necessary */
+  word_t var;   //to be scaned address
+  int old_value;
+
+} WP;
 
 #endif
